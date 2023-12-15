4aafda3a640505e639aee9f01ff174c24f1d0e4c
xuanchen <98302650+yaslydia@users.noreply.github.com>
2023-12-14 21:16:01 -0800

    scores

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,13 +1,14 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
-  { targetTime, remainingTime },
+  { targetTime, remainingTime, onReset },
   ref
 ) {
   const dialog = useRef();
 
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -20,6 +21,7 @@ const ResultModal = forwardRef(function ResultModal(
   return (
     <dialog ref={dialog} className="result-modal">
       {userLost && <h2>You lost</h2>}
+      {!userLost && <p>Your score is {score}</p>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
@@ -27,7 +29,7 @@ const ResultModal = forwardRef(function ResultModal(
         You stopped the timer with{" "}
         <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
